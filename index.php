0111719e4b3a5d07662fe1f032b7afef5459bce1
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-04 21:55:27 +0100

    add modal to add task('jQuery)

--- index.php
+++ index.php
@@ -21,9 +21,34 @@
             </center>
 
             <div class="col-md-10 col-md-offset-1">
-                <button type="button" class="btn btn-success btn-add">Add Task</button>
+                <button type="button" data-target="#myModal" data-toggle="modal" class="btn btn-success btn-add">Add
+                    Task</button>
                 <button type="button" class="btn btn-default pull-right btn-print">Print</button>
                 <hr><br>
+
+
+
+                <!-- Modal content-->
+                <div class="modal fade" id="myModal" role="dialog">
+                    <div class="modal-dialog">
+
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Modal Header</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>Some text in the modal.</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
                 <table class="table">
                     <thead>
                         <tr>
@@ -35,7 +60,13 @@
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>Mark</td>
+                            <td class="col-md-10">Mark</td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
                         </tr>
 
                     </tbody>
